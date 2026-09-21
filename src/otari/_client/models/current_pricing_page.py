0fd1d3d919627d7da13eea1278e8793f38e46062
khaledosman <2937633+khaledosman@users.noreply.github.com>
2026-09-21 09:15:29 +0000

    chore: regenerate SDK client core from Otari OpenAPI spec

--- src/otari/_client/models/current_pricing_page.py
+++ src/otari/_client/models/current_pricing_page.py
@@ -17,34 +17,20 @@ import pprint
 import re  # noqa: F401
 import json
 
-from pydantic import BaseModel, ConfigDict, StrictStr, field_validator
+from pydantic import BaseModel, ConfigDict, StrictInt
 from typing import Any, ClassVar, Dict, List
+from otari._client.models.pricing_response import PricingResponse
 from typing import Optional, Set
 from typing_extensions import Self
 from pydantic_core import to_jsonable_python
 
-class MRWebSearchToolResultError(BaseModel):
+class CurrentPricingPage(BaseModel):
     """
-    MRWebSearchToolResultError
+    One page of current model prices, with the total number of priced models.
     """ # noqa: E501
-    error_code: StrictStr
-    type: StrictStr
-    additional_properties: Dict[str, Any] = {}
-    __properties: ClassVar[List[str]] = ["error_code", "type"]
-
-    @field_validator('error_code')
-    def error_code_validate_enum(cls, value):
-        """Validates the enum"""
-        if value not in set(['invalid_tool_input', 'unavailable', 'max_uses_exceeded', 'too_many_requests', 'query_too_long', 'request_too_large']):
-            raise ValueError("must be one of enum values ('invalid_tool_input', 'unavailable', 'max_uses_exceeded', 'too_many_requests', 'query_too_long', 'request_too_large')")
-        return value
-
-    @field_validator('type')
-    def type_validate_enum(cls, value):
-        """Validates the enum"""
-        if value not in set(['web_search_tool_result_error']):
-            raise ValueError("must be one of enum values ('web_search_tool_result_error')")
-        return value
+    count: StrictInt
+    data: List[PricingResponse]
+    __properties: ClassVar[List[str]] = ["count", "data"]
 
     model_config = ConfigDict(
         validate_by_name=True,
@@ -64,7 +50,7 @@ class MRWebSearchToolResultError(BaseModel):
 
     @classmethod
     def from_json(cls, json_str: str) -> Optional[Self]:
-        """Create an instance of MRWebSearchToolResultError from a JSON string"""
+        """Create an instance of CurrentPricingPage from a JSON string"""
         return cls.from_dict(json.loads(json_str))
 
     def to_dict(self) -> Dict[str, Any]:
@@ -76,10 +62,8 @@ class MRWebSearchToolResultError(BaseModel):
         * `None` is only added to the output dict for nullable fields that
           were set at model initialization. Other fields with value `None`
           are ignored.
-        * Fields in `self.additional_properties` are added to the output dict.
         """
         excluded_fields: Set[str] = set([
-            "additional_properties",
         ])
 
         _dict = self.model_dump(
@@ -87,16 +71,18 @@ class MRWebSearchToolResultError(BaseModel):
             exclude=excluded_fields,
             exclude_none=True,
         )
-        # puts key-value pairs in additional_properties in the top level
-        if self.additional_properties is not None:
-            for _key, _value in self.additional_properties.items():
-                _dict[_key] = _value
-
+        # override the default output from pydantic by calling `to_dict()` of each item in data (list)
+        _items = []
+        if self.data:
+            for _item_data in self.data:
+                if _item_data:
+                    _items.append(_item_data.to_dict())
+            _dict['data'] = _items
         return _dict
 
     @classmethod
     def from_dict(cls, obj: Optional[Dict[str, Any]]) -> Optional[Self]:
-        """Create an instance of MRWebSearchToolResultError from a dict"""
+        """Create an instance of CurrentPricingPage from a dict"""
         if obj is None:
             return None
 
@@ -104,14 +90,9 @@ class MRWebSearchToolResultError(BaseModel):
             return cls.model_validate(obj)
 
         _obj = cls.model_validate({
-            "error_code": obj.get("error_code"),
-            "type": obj.get("type")
+            "count": obj.get("count"),
+            "data": [PricingResponse.from_dict(_item) for _item in obj["data"]] if obj.get("data") is not None else None
         })
-        # store additional fields in additional_properties
-        for _key in obj.keys():
-            if _key not in cls.__properties:
-                _obj.additional_properties[_key] = obj.get(_key)
-
         return _obj
 
 
